aed6bd49c0759f937685b5e53ae71094493690e4
PublicGameCoder <mikedegroot1999@hotmail.com>
2018-02-03 20:02:05 +0100

    Add MCMMO dependency

--- src/main/java/bbaddonmcmmo/BBAddonMCMMO.java
+++ src/main/java/bbaddonmcmmo/BBAddonMCMMO.java
@@ -2,23 +2,28 @@ package bbaddonmcmmo;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.gmail.nossr50.mcMMO;
+
 import managers.ConfigManager;
 import managers.DataBaseManager;
 import me.bigteddy98.bannerboard.api.BannerBoardAPI;
 import me.bigteddy98.bannerboard.api.BannerBoardManager;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class BBAddonMCMMO extends JavaPlugin {
 	
 	private static BBAddonMCMMO instance;
-	private BannerBoardAPI bbm;
 	
 	@Override
 	public void onEnable() {
 		instance = this;
-		this.bbm = BannerBoardManager.getAPI();
+		
+		mcMMO mcmmo = (mcMMO) getServer().getPluginManager().getPlugin("mcMMO");
+		if (mcmmo == null || !mcmmo.isEnabled()) {
+			System.out.println("mcMMO is not included or is not enabled!");
+			getServer().getPluginManager().disablePlugin(this);
+			return;
+		}
+		
 		ConfigManager.getManager();
 		DataBaseManager.getManager();
 		System.out.println("BannerBoard-McMMO-Addon is enabled!");
@@ -29,6 +34,6 @@ public class BBAddonMCMMO extends JavaPlugin {
 	}
 	
 	public BannerBoardAPI getBannerBoardAPI() {
-		return bbm;
+		return BannerBoardManager.getAPI();
 	}
 }
